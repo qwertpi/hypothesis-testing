ad132927e549b845c948b5c406d1402bc69b1996
qwertpi <qwertpi@users.noreply.github.com>
2020-12-23 10:02:19 +0000

    Made critical region more efficent
    By using binary search

--- main.scala
+++ main.scala
@@ -2,75 +2,95 @@ import io.StdIn.readLine
 import annotation.tailrec
 import scala.math.Ordering.Double.TotalOrdering
 
-object HypothesisTesting extends App {
-    class Binomial(test_stat: Int, num_trials: Int, trial_prob: Double, sig_level: Double, tail: String) {
-        @tailrec
-        //bigint is required beacuse factorial is very big for moderately large n, who knew?
-        private def limited_factorial(n: BigInt, lim: BigInt, result: BigInt = 1): BigInt ={
-            if (n <= lim) result else limited_factorial(n-1, lim, result*n)
+class Binomial(test_stat: Int, num_trials: Int, trial_prob: Double, sig_level: Double, tail: String) {
+    @tailrec
+    //bigint is required beacuse factorial is very big for moderately large n, who knew?
+    private def limited_factorial(n: BigInt, lim: BigInt, result: BigInt = 1): BigInt ={
+        if (n <= lim) result else limited_factorial(n-1, lim, result*n)
+    }
+    private def factorial(n: Int): BigInt ={
+        return limited_factorial(n, 1)
+    }
+    //returns BigDecimal because it's going to be multiplied with more decimals
+    private def num_combinations(n: Int, r: Int): BigDecimal ={
+        return BigDecimal(limited_factorial(n, r)/factorial(n-r))
+    }
+
+    private def probability(x: Int): BigDecimal ={
+        val p: BigDecimal = BigDecimal(trial_prob)
+        return num_combinations(num_trials, x) * p.pow(x) * (1-p).pow(num_trials-x)
+    }
+    //calculates less than or equal to
+    def less_than_probability(): Double ={
+        return (0 to test_stat).map(probability(_: Int)).sum.toDouble
+    }
+    //calculates greater than or equal to
+    def greater_than_probability(): Double ={
+        return (test_stat to num_trials).map(probability(_: Int)).sum.toDouble
+    }
+
+    @tailrec
+    private def calculate_critical_region(lower: Int = 0, upper: Int = num_trials): Int ={
+        val middle: Int = lower + (upper - lower)/2
+        Thread.sleep(500)
+        println((new Binomial(middle, num_trials, trial_prob, sig_level, tail)).calc_p())
+        if (upper - lower <= 1){
+            if (((new Binomial(lower, num_trials, trial_prob, sig_level, tail)).calc_p()) < sig_level) return lower else return upper
         }
-        private def factorial(n: Int): BigInt ={
-            return limited_factorial(n, 1)
+        if (((new Binomial(middle, num_trials, trial_prob, sig_level, tail)).calc_p()) > sig_level){
+            if (tail == "less") return calculate_critical_region(lower, middle) else return calculate_critical_region(middle, upper)
         }
-        //returns BigDecimal because it's going to be multiplied with more decimals
-        private def num_combinations(n: Int, r: Int): BigDecimal ={
-            return BigDecimal(limited_factorial(n, r)/factorial(n-r))
+        else{
+            if (tail == "greater") return calculate_critical_region(lower, middle) else return calculate_critical_region(middle, upper)
+        }
+    }
+    //unit means we don't return anything
+    def get_critical_region(): Unit ={
+        if (tail == "greater"){
+            val critical_value: Int = calculate_critical_region()
+            println(s"Critical region is X≥$critical_value")
+        }
+        else if (tail == "less"){
+            val critical_value: Int = calculate_critical_region()
+            println(s"Critical region is X≤$critical_value")
+        }
+        else {
+
+        }
+    }
+    def pretty_calc_p(): Double = {
+        if (tail == "greater") println("Calculating P(X≥a)") else if (tail == "less") println("Calculating P(X≤a)")
+        return calc_p()
+    }
+    private def calc_p(): Double ={
+        if (tail == "greater") {
+            return greater_than_probability()
+        }
+        else if (tail == "less") {
+            return less_than_probability()
         }
 
-        private def probability(x: Int): BigDecimal ={
-            val p: BigDecimal = BigDecimal(trial_prob)
-            return num_combinations(num_trials, x) * p.pow(x) * (1-p).pow(num_trials-x)
-        }
-        //calculates less than or equal to
-        def less_than_probability(): Double ={
-            return (0 to test_stat).map(probability(_: Int)).sum.toDouble
-        }
-        //calculates greater than or equal to
-        def greater_than_probability(): Double ={
-            return (test_stat to num_trials).map(probability(_: Int)).sum.toDouble
-        }
+        else {
+            println("Conducting a two-tailed test")
+            println("Halving significance level")
 
-        //unit means we don't return anything
-        def critical_region(): Unit ={
-            if (tail == "greater"){
-                val critical_value: Int = (0 to num_trials).map(x => (new Binomial(x, num_trials, trial_prob, sig_level, tail).greater_than_probability(), x)).toMap.view.filterKeys(_ < sig_level).max._2
-                println(s"Critical region is X≥$critical_value")
+            val expected_mean: Double = num_trials * trial_prob
+            println(s"Expected mean value of sample $expected_mean")
+            if (test_stat > expected_mean) {
+                println("Number of successes is greater than mean value")
+                val new_b: Binomial = new Binomial(test_stat, num_trials, trial_prob, sig_level, "greater")
+                new_b.get_critical_region()
+                return new_b.pretty_calc_p()
             }
-            else if (tail == "less"){
-                val critical_value: Int = (0 to num_trials).map(x => (new Binomial(x, num_trials, trial_prob, sig_level, tail).less_than_probability(), x)).toMap.view.filterKeys(_ < sig_level).max._2
-                println(s"Critical region is X≤$critical_value")
-            }
-        }
-        def calculate_p(): Double ={
-            if (tail == "greater") {
-                println("Calculating P(X≥a)")
-                critical_region()
-                return greater_than_probability()
-            }
-            else if (tail == "less") {
-                println("Calculating P(X≤a)")
-                critical_region()
-                return less_than_probability()
-            }
-
             else {
-                println("Conducting a two-tailed test")
-                println("Halving significance level")
-
-                val expected_mean: Double = num_trials * trial_prob
-                println(s"Expected mean value of sample $expected_mean")
-                if (test_stat > expected_mean) {
-                    println("Number of successes is greater than mean value")
-                    return (new Binomial(test_stat, num_trials, trial_prob, sig_level, "greater")).calculate_p()
-                }
-                else {
-                    println("Number of successes is less than mean value")
-                    (new Binomial(test_stat, num_trials, trial_prob, sig_level, "less")).calculate_p()
-                }
+                println("Number of successes is less than mean value")
+                (new Binomial(test_stat, num_trials, trial_prob, sig_level, "less")).pretty_calc_p()
             }
         }
     }
+}
 
+object HypothesisTesting extends App {
 	def adjust_sig(sig: Double, tail: String): Double ={if (tail != "two tail") sig else sig/2}
 
 	val test_type: String = (readLine("Under the alternative hypothesis has the probability of success: 1) Increased 2) Decreased 3) Don't know  ")) match {case "1" => "greater" case "2" => "less" case "3" => "two tail"}
@@ -81,11 +101,13 @@ object HypothesisTesting extends App {
 	println("")
 
 	println("")
-	val p: Double = (new Binomial(sample_successes, sample_size, trial_prob, sig_level, test_type)).calculate_p()
+    val b: Binomial = new Binomial(sample_successes, sample_size, trial_prob, sig_level, test_type)
+	val p: Double = b.pretty_calc_p()
 	if (p > sig_level) {
 		println(s"p=$p, insufficient evidence to reject null hypothesis")
 	}
 	else {
 		println(s"p=$p, sufficient evidence to reject null hypothesis")
 	}
+    b.get_critical_region()
 }
